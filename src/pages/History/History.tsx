637a9fba09b6a83107b9d00992c5a150fb4c1c81
Robson <robsonleitao26@gmail.com>
2024-02-27 20:12:44 -0300

    trabalhando no formulario com o hook useForm

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,5 +1,35 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
     return (
-        <h1>history</h1>
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>uma tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,42 @@
 import { Play } from "phosphor-react";
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import { useForm } from "react-hook-form";
 
 export function Home() {
+    const {register, handleSubmit, watch} = useForm()
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
+    function handleCreateNewCycle(data: any) {
+        console.log(data)
+    }
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput type="text" id="task" />
+                    <TaskInput 
+                        type="text" 
+                        id="task" 
+                        placeholder="Dê um nome para o seu projeto"
+                        list="task-suggestions"
+                        {...register('task')}
+                    />
+
+                    <datalist id="task-suggestions">
+                        <option value="projeto 222" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" />
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        step={5}
+                        min={0}
+                        max={60}
+                        {...register('minutesAmount', {valueAsNumber: true})}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -23,7 +49,7 @@ export function Home() {
                     <span>0</span>
                 </CountDownContainer>
 
-                <StartCountDownButton type="submit">
+                <StartCountDownButton type="submit" disabled={isSubmitDisabled}>
                     <Play size={24} />
                     Começar
                 </StartCountDownButton>
